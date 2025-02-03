46f6427b1734045c70bafb8c83f59b8886190289
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-02-03 21:39:22 +0530

    Introcution to Generic Trees

---
+++ Generic Tree/Intro.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Intro {
+    private class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 50, -1, 60, -1, 70, -1, -1, 30, 80, -1, -1, 40, 100, -1, 90, -1, -1, -1};
+        
+        Stack<Node> st = new Stack<>();
+        Node root;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                
+                t.data = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(t);
+                    st.push(t);
+                } else {
+                    root = t;
+                }
+            }
+        }
+    }
+}
